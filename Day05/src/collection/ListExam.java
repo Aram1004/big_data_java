eb8044fae3d28ed8df26dab98833f6065c8c56c2
unknown <2060700@naver.com>
2019-01-17 21:33:40 +0900

    Update refactoring

--- Day05/src/collection/ListExam.java
+++ Day05/src/collection/ListExam.java
@@ -1,56 +1,55 @@
 package collection;
+
 import java.util.ArrayList;
 
 public class ListExam {
 
-	public static void testList(){
-		ArrayList<String>  list = new ArrayList<String>(); // 배열이 생김으로 index를 사용해서 쓴다.
-		
-//		추가  (중복 상관없이 다 추가 가능)
-		list.add("홍길동");//0
-		list.add("김철수");//1
-		list.add("박영희");//2
-		list.add("이순이");//3
-		list.add("이순이");//4
-		
-		System.out.println("list의 size : "+list.size());  //총 개수 알려줌
+	public static void testList() {
+		ArrayList<String> list = new ArrayList<String>(); // 배열이 생김으로 index를 사용해서 쓴다.
+
+		// 추가 (중복 상관없이 다 추가 가능)
+		list.add("홍길동");// 0
+		list.add("김철수");// 1
+		list.add("박영희");// 2
+		list.add("이순이");// 3
+		list.add("이순이");// 4
+
+		System.out.println("list의 size : " + list.size()); // 총 개수 알려줌
 		System.out.println(list);
 
-//		삽입  (중간에 넣는것)
-		list.add(1, "박지우");//1번 index에 "박지우" 을 삽입. (오버로딩되있음)
+		// 삽입 (중간에 넣는것)
+		list.add(1, "박지우");// 1번 index에 "박지우" 을 삽입. (오버로딩되있음)
 		System.out.println(list);
 
-//		삭제
+		// 삭제
 		list.remove("이순이");// 삭제할 객체 전
 		System.out.println(list);
-//		
-		String o = list.remove(1); //삭제할것에 index를 주는것 / 삭제할 객체의 index를 전달.
-		//삭제한 객체(원소) 반환.
+		//
+		String o = list.remove(1); // 삭제할것에 index를 주는것 / 삭제할 객체의 index를 전달.
+		// 삭제한 객체(원소) 반환.
 		System.out.println(o); // 삭제된것을 알기위해 변수로 받아 삭제된것을 찍어보기
 		System.out.println(list);
 
-//		조회 -- get(조회할 index)
+		// 조회 -- get(조회할 index)
 		o = list.get(1);
-		System.out.println("1번 idx 조회결과 : "+o);
-		
-//		변경
-		o = list.set(1, "오영길");
-		System.out.println("대체된 객체 : "+o);
+		System.out.println("1번 idx 조회결과 : " + o);
 
+		// 변경
+		o = list.set(1, "오영길");
+		System.out.println("대체된 객체 : " + o);
 
 		list.add("박영희");
 		System.out.println(list);
 		System.out.println("박영희 객체의 index(앞에서부터) : " + list.indexOf("박영희"));
 		System.out.println("박영희 객체의 index(뒤에서부터) : " + list.lastIndexOf("박영희"));
-		
 
 		System.out.println("-------반복문을 이용한 조회------------");
-		for(int i = 0; i < list.size(); i++ ){  //리스트에 있는 원소 개수
-			
-			String name = list.get(i);  //get의 i번째 list값
+		for (int i = 0; i < list.size(); i++) { // 리스트에 있는 원소 개수
+
+			String name = list.get(i); // get의 i번째 list값
 			System.out.println(name);
 		}
-		for(String n : list) {   //향샹된 for문
+		for (String n : list) { // 향샹된 for문
 			System.out.println(n);
 		}
 	}
@@ -59,14 +58,3 @@ public class ListExam {
 		testList();
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
--- Day05/src/collection/MapExam.java
+++ Day05/src/collection/MapExam.java
@@ -1,58 +1,48 @@
 package collection;
+
 import java.util.HashMap;
 import java.util.Set;
 
 public class MapExam {
 
-	public static void testMap(){
-		
-		HashMap<String, Object> map = new HashMap<>(); //객체에 이름을 붙여줄껀데 string값으로 붙여줄거란말
-	
+	public static void testMap() {
+
+		HashMap<String, Object> map = new HashMap<>(); // 객체에 이름을 붙여줄껀데 string값으로 붙여줄거란말
+
 //		추가 - put(key, value)  key값은 중복을 허용하지 않는다. value는 가능, 추가 변경을 둘다 함 put  == key-value 값을 쌍으로 추가해야한다
-		map.put("name", "김영수"); 
+		map.put("name", "김영수");
 		map.put("age", 20);
 		map.put("weight", 70.5);
 		map.put("address", "경기도 성남시 분당구");
 		System.out.println(map);
 
 //		값 조회  - ge(키)
-		Object name = map.get("name");  //키 값을 입력하면 그에따른 value값을 찾아준다.
-		System.out.println("이름 : "+name);
-		
+		Object name = map.get("name"); // 키 값을 입력하면 그에따른 value값을 찾아준다.
+		System.out.println("이름 : " + name);
+
 //		value를 변경  - put(key,value)
-		// key값 있으면 변경, key가 없으면 추가 이다. 
+		// key값 있으면 변경, key가 없으면 추가 이다.
 		Object oldAge = map.put("age", 25);
-		System.out.println("변경 전 나이 : "+oldAge);
+		System.out.println("변경 전 나이 : " + oldAge);
 //
 		map.put("name", "이영수");
 		System.out.println(map);
 
 //		삭제 - remove(키)
 		Object delName = map.remove("name");
-		System.out.println("삭제된 이름 : "+delName);
+		System.out.println("삭제된 이름 : " + delName);
 		System.out.println(map);
 
 //		반복문을 이용한 조회 : KEY값들을 조회한 뒤 KEY값들을 반복문을 이용해 처리
 		Set<String> keys = map.keySet();
-		for(String key : keys){
+		for (String key : keys) {
 			System.out.println(key);
 			Object value = map.get(key);
 			System.out.println(value);
 		}
 	}
 
-	
 	public static void main(String[] args) {
 		testMap();
 	}
 }
-
-
-
-
-
-
-
-
-
-
